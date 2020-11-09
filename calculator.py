dc6a1614d4a90135b1f874dd805dc6e2bffebbf3
sekwangoh <osktr5@gmail.com>
2020-11-09 16:33:23 +0900

    create Calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+def add(num1,num2):
+  return num1 + num2
+
+print(add(1,2))
+
+def subtract(num1,num2):
+  return num1 - num2
+
+print(subtract(1,2))
+
+def multiplication(num1,num2):
+  return num1 * num2
+
+print(multiplication(1,2))
+
+def division(num1, num2):
+  return num1 / num2
+
+print(division(4,2))
+
+
